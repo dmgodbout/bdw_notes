835ee805309de827b89a57825e43a9e05902a1e4
Davis Godbout <dmgodbout@gmail.com>
2012-02-09 12:37:55 -0700

    homework

---
+++ app/models/note.rb
@@ -0,0 +1,5 @@
+class Note < ActiveRecord::Base
+  validates :name,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MYFIRSTAPP::Application.config.secret_token = '2c85f5713788cc266ac81732933b382bf13b85680014ecb1a7955cda4ff17fed4d4781bee6841f2362653db8c8f2f64b631dc2f83b32ab35be712b54124d1222'
